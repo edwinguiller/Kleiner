21b83b850d1440c423e5c3c7e01beaa474f03523
joeleboucher-leboss <baptiste.bessodes@gmail.com>
2020-04-09 17:38:17 +0200

    jinja

--- AgiWeb/run.py
+++ AgiWeb/run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +9,16 @@ def index():
 @app.route("/AgiLean_accueil")
 def leanacc():
 	return render_template("AgiLean_accueil.html")
+
+@app.route("/AgiLog_accueil")
+def logacc():
+	return render_template("AgiLog_accueil.html")
+
+@app.route("/initialisation")
+def initialisation():
+	conn = sqlite3.connect("AgiWeb_BDD.db")
+	cursor = conn.cursor()
+	cursor.execute("select id_piece from piece")
+	list_id = cursor.fetchall()
+	conn.close()
+	return render_template("initialisation.html", list_id_piece=list_id)
