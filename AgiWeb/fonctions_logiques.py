ddbc5aeed5f0be428e8b73ff114dc77aa3dd68d7
lekanak <62421241+lekanak@users.noreply.github.com>
2020-04-23 12:04:30 +0200

    maj_commande

--- AgiWeb/fonctions_logiques.py
+++ AgiWeb/fonctions_logiques.py
@@ -38,31 +38,31 @@ def creer_id(b):#créé un id
     return(ide)
 
 def demande_interaction(n,contenu):
-	for i in range(n):
-		contenu += "<form method='post' action='code_kit'>"
-		contenu += "<input type='str' name='nom_kit'+str(i)+'' value='' />"
-	contenu += "<input type='submit' value='Envoyer'/> </form>"
-	return(contenu)
+    for i in range(n):
+        contenu += "<form method='post' action='code_kit'>"
+        contenu += "<input type='str' name='nom_kit'+str(i)+'' value='' />"
+    contenu += "<input type='submit' value='Envoyer'/> </form>"
+    return(contenu)
 def recupere_interraction(n,contenu):
-	L=[]
-	for i in range(n):
-		nom=str(request.args.get('nom_kit'+str(i),''))
-		L.append(nom)
-	return(L)
+    L=[]
+    for i in range(n):
+        nom=str(request.args.get('nom_kit'+str(i),''))
+        L.append(nom)
+    return(L)
 
 def modifier_kit(nom_kit,pieces,quantites):#La piece est choisit parmit un menu dérouant donc il n'y a pas besoin de vérifier
-	return(ajouter_piece(compo_kit, [piece,quantite], [pieces,quantites], [str(),int()]))
+    return(ajouter_piece(compo_kit, [piece,quantite], [pieces,quantites], [str(),int()]))
 
 def quantite_bonne(quantite):
-	try: #on vérifie que la quantité est bonne
-			quantite=int(quantite)
-	except: #si la quantité est mauvaise alors message d'erreur
-		return(print([quantite,False]))
-	else:
-		if quantite>0:
-			return(print([quantite,True]))
-		else:
-			return(print([quantite,False]))
+    try: #on vérifie que la quantité est bonne
+            quantite=int(quantite)
+    except: #si la quantité est mauvaise alors message d'erreur
+        return(print([quantite,False]))
+    else:
+        if quantite>0:
+            return(print([quantite,True]))
+        else:
+            return(print([quantite,False]))
 quantite_bonne(0)
 
 
@@ -168,7 +168,7 @@ def mise_a_jour_bdd (base, colonne, entree, types): # prend en argument  une bas
     con.commit()
     con.close()
 
-def seuil_commande (stock,seuil_recomp,nom): #stock, nom et seuil_recomp sont des listes et si les stock sont sup au seuil, la colonne a_commander de la pièce passe à 1
+def seuil_commande (stock,seuil_recomp,nom): #stock, nom et seuil_recomp sont des listes et si les stock sont inf au seuil, la colonne a_commander de la pièce passe à 1
 
     con = lite.connect(cheminbdd) #attention chez toi c'est pas rangé au meme endroit
     con.row_factory = lite.Row
@@ -202,3 +202,92 @@ def tableau (base,colonne): #prend les infos d'une base, et les rentres dans un 
         listes=liste(cur.fetchall())
         tableau.append(listes)
     return tableau
+def tab (listesql): #prend une liste sql et la transforme en tableau
+
+    tt=[]
+    for i in listesql:
+        a=[]
+        for j in i:
+            a.append(j)
+        tt.append(a)
+    ttt=[0]*len(tt[0])
+    for i in range (0,len(tt[0])):
+        ttt[i]=[0]*len(tt)
+    for i in range (0,len(tt[0])):
+            for j in range (0,len(tt)):
+                ttt[i][j]=tt[j][i]
+    return(ttt)
+def convert_dict(L,c1=None,c2=None,c3=None,c4=None,c5=None,c6=None,c7=None,c8=None): #ci colonnes, ce sont des strings
+    D=dict()
+    C=[c1,c2,c3,c4,c5,c6,c7,c8]
+    for i in range(8):
+        if type(C[i])==str:
+            D[C[i]]=L[i]
+    return D
+
+def select_encours ():
+
+    con = lite.connect(cheminbdd)
+    con.row_factory = lite.Row
+    cur = con.cursor()
+    cur.execute("SELECT id_com, date,nom as nom_piece,quantite, strftime('%s',date_arrivee)- strftime('%s',strftime('%H:%M:%S','now')) as timer from (SELECT id_com,nom,quantite,date,delai,strftime('%H:%M:%S',(SELECT commande.date FROM commande),delai) as date_arrivee from (SELECT piece.id as id_piece,piece.nom, piece.fournisseur, fournisseur.delai from piece join fournisseur ON piece.fournisseur==fournisseur.id) JOIN (SELECT commande.id as id_com,commande.date,compo_commande.piece,compo_commande.quantite from commande join compo_commande on commande.id==compo_commande.commande) ON id_piece==piece);")
+    d=tab(cur.fetchall())
+    b=convert_dict(d,"id","date","nom", "quantite","timer")
+    con.close
+
+    return b
+
+def select_stock_reel ():
+
+    con = lite.connect(cheminbdd)
+    con.row_factory = lite.Row
+    cur = con.cursor()
+    cur.execute("SELECT id, nom, quantite, a_commander FROM piece")
+    d=tab(cur.fetchall())
+    b=convert_dict(d,"id","nom","quantite", "a_commander")
+    for i in range (0,len(b["a_commander"])):
+        if (b["a_commander"][i]==1):
+            b["a_commander"][i]="OUI"
+        elif (b["a_commander"][i]==0):
+            b["a_commander"][i]="NON"
+        else:
+            b["a_commander"][i]="ERREUR"
+    con.close
+
+    return b
+
+def select_commande_fournisseur (fournisseur): #prend le fournisseur en lettre minuscule et("agigreen" ou "agipart") et renvoi les id, nom et quantité des pièces à commander du fournisseur
+    #fonctionne uniquement si il y'a quelque chose dans la colonne à commander (0 ou 1) pour les 2 fournisseur mais pas si il y'a que des NULL, si il y a que des NULL c'est chiant faut que je fasse un test pour ca
+    con = lite.connect(cheminbdd)
+    con.row_factory = lite.Row
+    cur = con.cursor()
+
+    if (transformation(fournisseur)=="agigreen"):
+        fourniss=1
+    elif (transformation(fournisseur)=="agipart"):
+        fourniss=2
+    else:
+        return ERREUR
+
+    cur.execute("SELECT id, nom, quantite FROM piece WHERE a_commander=? and fournisseur=?", [1,fourniss])
+    d=tab(cur.fetchall())
+    b=convert_dict(d,"id","nom","quantite")
+    con.close
+
+    return b
+
+def passer__commande(commande): #prend en argument le dictionnaire commande avec les id des pieces, leurs nom et la quantite pas fini
+
+    con = lite.connect(cheminbdd)
+    con.row_factory = lite.Row
+    cur = con.cursor()
+    # générer id
+    cur.execute ("SELECT id FROM commande")
+    liste_id=(cur.fetchall())
+    newid=creer_id(liste_id)
+    # creation de la commande
+    cur.execute("INSERT INTO commande (id,date,reception) VALUES (?,?,?)", (new_id,CURRENT_TIMESTAMP,0))
+    for i in range (0,commande["id"]):
+        cur.execute("INSERT INTO compo_commande(commande,piece,quantite) VALUES(?,?,?)", (newid, commande["id"][i], commande["quantite"][i]))
+
+
